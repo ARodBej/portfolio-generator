b1a252378191240391028a30723776b6b0d353f7
Adilene Bejines <adi3511@hotmail.com>
2022-01-03 01:04:45 -0700

    worked with arrow functions

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const printPortfileData = profileDataArr => {
+    //this..
+    for (let i = 0; i < profileDataArr.length; i+= 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log("================");
+
+    //is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printPortfileData(profileDataArgs);
